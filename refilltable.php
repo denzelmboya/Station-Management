c5a69f53d56c47be5bf44148cb0282bc51a1a59e
Ŕenzel <38043903+denzelmboya@users.noreply.github.com>
2019-08-09 20:08:50 +0300

    Update refilltable.php

--- refilltable.php
+++ refilltable.php
@@ -11,8 +11,8 @@
 <?php
 $servername = "localhost";
 $dbusername = "root";
-$dbpassword = "33587279Magic6666";
-$dbname = "petroldb";
+$dbpassword = "";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
