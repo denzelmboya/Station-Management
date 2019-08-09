009898749e732cbee0035aac04a99d0ca2878e69
Ŕenzel <38043903+denzelmboya@users.noreply.github.com>
2019-08-09 20:10:17 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,8 +1,8 @@
 <?php
 $servername="localhost";
 $dbusername="root";
-$dbpassword="33587279Magic6666";
-$dbname="petroldb";
+$dbpassword="";
+$dbname="";
 
 $position = $_POST ['position'];
 $fname = $_POST ['fname'];
